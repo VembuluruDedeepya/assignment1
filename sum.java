07936ecf56a7514f7c92d310acf91dfbbcefb120
VembuluruDedeepya <129401587+VembuluruDedeepya@users.noreply.github.com>
2023-03-30 20:16:02 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class sum
+{
+  void add(int x,int y)
+  {
+    add=x+y;
+    System.out.println(add);
+  }
+  public static void main(String[] args)
+  {
+    Scanner s=new Scanner(System.in);
+    int x,y;
+    x=s.nextInt();
+    y=s.nextInt();
+    sum t=new sum();
+    t.add();
+  }
+}
+    
+   
+    
+    
